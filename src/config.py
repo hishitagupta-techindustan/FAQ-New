b4e9bf40d49de42cce1e023f8060518fcb4bf4da
HishitaGupta <hishitagupta.10@gmail.com>
2026-03-02 16:31:50 +0530

    env added

--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ import os
 from pathlib import Path
 from typing import Optional
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class Settings(BaseSettings):
