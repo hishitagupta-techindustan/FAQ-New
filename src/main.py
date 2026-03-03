5549fbe719902c8a1dcfba5efe3f7e1b3c0aed3f
HishitaGupta <hishitagupta.10@gmail.com>
2026-03-03 11:42:43 +0530

    main file corrected

--- src/main.py
+++ src/main.py
@@ -336,6 +336,6 @@ def ingest_pdf(
 # ---------------------- ENTRY ------------------------
 # =====================================================
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
